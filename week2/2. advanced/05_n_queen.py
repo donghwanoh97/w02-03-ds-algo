18d8c7c935e24b5d25cb8a8ce5e95edb97443b9c
donghwanoh97 <odh9568@gmail.com>
2026-09-01 15:17:16 +0900

    05_n_queen 해결

--- week2/2. advanced/05_n_queen.py
+++ week2/2. advanced/05_n_queen.py
@@ -62,13 +62,33 @@ def n_queens(n: int) -> int:
     """
     # TODO: 백트래킹으로 가능한 배치의 수를 반환하세요.
     # 권장 구조:
-    #   cols = [0] * n
-    #   count = 0
-    #   def place(row):
-    #       ...
-    #   place(0)
-    #   return count
-    pass
+    cols = [0] * n
+    count = 0
+
+    def place(row):
+        nonlocal count
+
+        if row == n:
+            count += 1
+            return 
+        
+        # 0부터 n까지 열 중에서 퀸을 배치할 곳 탐색
+        for i in range(n):
+          is_valid = True
+
+          # 현재까지 배치된 퀸(cols[j])과 서로 공격하지 않는다면
+          for j in range(row):
+              if cols[j] == i or abs(cols[j] - i) ==  row - j:
+                  is_valid = False
+                  break          
+            
+          # 퀸 배치
+          if is_valid:
+              cols[row] = i
+              place(row+1)
+        
+    place(0)
+    return count
 
 
 if __name__ == "__main__":
